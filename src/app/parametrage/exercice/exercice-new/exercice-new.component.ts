c615e86b5611fd7f90cc3ac2d55e11a7c87b7352
Saliou.lo <zalesalioulo@gmail.com>
2020-01-23 13:51:06 +0000

    create service pour la convertion de la date

--- src/app/parametrage/exercice/exercice-new/exercice-new.component.ts
+++ src/app/parametrage/exercice/exercice-new/exercice-new.component.ts
@@ -1,3 +1,4 @@
+import { ConvertDateService } from './../../../shared/services/convert-date.service';
 import { Component, OnInit } from '@angular/core';
 import { Exercice } from '../exercice';
 import { ExerciceService } from '../exercice.service';
@@ -14,6 +15,7 @@ export class ExerciceNewComponent implements OnInit {
   exercice: Exercice;
   constructor(public exerciceSrv: ExerciceService,
     public notificationSrv: NotificationService,
+    public convertDateServiceSrv: ConvertDateService, 
     public router: Router, public location: Location) {
     this.exercice = new Exercice();
   }
@@ -22,6 +24,9 @@ export class ExerciceNewComponent implements OnInit {
   }
 
   saveExercice() {
+    this.exercice.dateDebut = this.convertDateServiceSrv.formatDate(this.exercice.dateDebut);
+    this.exercice.dateFin = this.convertDateServiceSrv.formatDate(this.exercice.dateFin);
+    console.log(this.exercice.dateDebut);
     this.exerciceSrv.create(this.exercice)
       .subscribe((data: any) => {
         this.notificationSrv.showInfo('Exercice créé avec succès');
@@ -30,6 +35,9 @@ export class ExerciceNewComponent implements OnInit {
   }
 
   saveExerciceAndExit() {
+    this.exercice.dateDebut = this.convertDateServiceSrv.formatDate(this.exercice.dateDebut);
+    this.exercice.dateFin = this.convertDateServiceSrv.formatDate(this.exercice.dateFin);
+    console.log(this.exercice.dateDebut);
     this.exerciceSrv.create(this.exercice)
       .subscribe((data: any) => {
         this.router.navigate([this.exerciceSrv.getRoutePrefix(), data.id]);
