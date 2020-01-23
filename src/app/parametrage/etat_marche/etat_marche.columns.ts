0def62b014be0e3009476eaf61653c3e891fc838
Ahmadou Gueye <elhadj.gueye@teqlab.tech>
2020-01-23 10:37:45 +0000

    added etat suivant column in etat-marche-list

--- src/app/parametrage/etat_marche/etat_marche.columns.ts
+++ src/app/parametrage/etat_marche/etat_marche.columns.ts
@@ -2,12 +2,14 @@ const etat_marcheColumns = [
             { header: 'Code', field: 'code', dataKey: 'code' },
             { header: 'Libelle', field: 'libelle', dataKey: 'libelle' },
             { header: 'Description', field: 'description', dataKey: 'description' },
+            { header: 'Etat Suivant', field: 'etatSuivant', dataKey: 'etatSuivant' },
         ];
 
 const allowedEtatMarcheFieldsForFilter = [
     'code',
     'libelle',
     'description',
+    'etatSuivant'
 ];
 
 export { etat_marcheColumns,allowedEtatMarcheFieldsForFilter };
