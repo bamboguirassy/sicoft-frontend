4a7768191b1cf648e62590dbf47c7ac47dde2cab
dofbi <amdymila@gmail,com>
2020-01-24 11:22:25 +0000

    validation formulaire secteur

--- src/app/parametrage/secteur/secteur.routes.ts
+++ src/app/parametrage/secteur/secteur.routes.ts
@@ -1,4 +1,4 @@
-import { Route } from "@angular/router";
+import { Route } from '@angular/router';
 import { SecteurListComponent } from './secteur-list/secteur-list.component';
 import { SecteurNewComponent } from './secteur-new/secteur-new.component';
 import { SecteurEditComponent } from './secteur-edit/secteur-edit.component';
@@ -8,14 +8,41 @@ import { MultipleSecteurResolver } from './multiple-secteur.resolver';
 import { OneSecteurResolver } from './one-secteur.resolver';
 
 const secteurRoutes: Route = {
-    path: 'secteur', children: [
-        { path: '', component: SecteurListComponent, resolve: { secteurs: MultipleSecteurResolver } },
-        { path: 'new', component: SecteurNewComponent },
-        { path: ':id/edit', component: SecteurEditComponent, resolve: { secteur: OneSecteurResolver } },
-        { path: ':id/clone', component: SecteurCloneComponent, resolve: { secteur: OneSecteurResolver } },
-        { path: ':id', component: SecteurShowComponent, resolve: { secteur: OneSecteurResolver } }
-    ]
+  path: 'secteur',
+  children: [
+    {
+      path: '',
+      component: SecteurListComponent,
+      resolve: { secteurs: MultipleSecteurResolver }
+    },
+    {
+      path: 'new',
+      component: SecteurNewComponent,
+      resolve: { secteurs: MultipleSecteurResolver }
+    },
+    {
+      path: ':id/edit',
+      component: SecteurEditComponent,
+      resolve: {
+          secteur: OneSecteurResolver,
+          secteurs: MultipleSecteurResolver
+         }
+    },
 
+    {
+      path: ':id/clone',
+      component: SecteurCloneComponent,
+      resolve: {
+        secteur: OneSecteurResolver,
+        secteurs: MultipleSecteurResolver
+      }
+    },
+    {
+      path: ':id',
+      component: SecteurShowComponent,
+      resolve: { secteur: OneSecteurResolver }
+    }
+  ]
 };
 
-export { secteurRoutes }
+export { secteurRoutes };
