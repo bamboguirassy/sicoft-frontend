980c2a70032cae679934006b2dac3cd9f5b71882
Aminata13 <59060784+Aminata13@users.noreply.github.com>
2020-01-30 20:12:15 +0000

    Mise à jour su show de Secteur

--- src/app/parametrage/secteur/secteur.ts
+++ src/app/parametrage/secteur/secteur.ts
@@ -1,6 +1,9 @@
+import { Fournisseur } from '../fournisseur/fournisseur';
+
 export class Secteur {
   id: any;
   code: string;
   libelle: string;
   description: string;
+  fournisseurs: Fournisseur[];
 }
