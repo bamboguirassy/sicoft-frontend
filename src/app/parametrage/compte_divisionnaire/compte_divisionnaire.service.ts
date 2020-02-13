b6bb84e7bff7ec4503dd5069af77533422fc0952
dofbi <amdymila@gmail,com>
2020-02-13 15:24:33 +0000

    récupéreration  les enregistrements à partir de l'objet parent

--- src/app/parametrage/compte_divisionnaire/compte_divisionnaire.service.ts
+++ src/app/parametrage/compte_divisionnaire/compte_divisionnaire.service.ts
@@ -25,19 +25,19 @@ export class CompteDivisionnaireService {
   }
 
   update(compte_divisionnaire: CompteDivisionnaire) {
-    return this.httpSrv.put(this.getRoutePrefixWithSlash()+compte_divisionnaire.id+'/edit', compte_divisionnaire);
+    return this.httpSrv.put(this.getRoutePrefixWithSlash() + compte_divisionnaire.id + '/edit', compte_divisionnaire);
   }
 
   clone(original: CompteDivisionnaire, clone: CompteDivisionnaire) {
-    return this.httpSrv.put(this.getRoutePrefixWithSlash()+original.id+'/clone', clone);
+    return this.httpSrv.put(this.getRoutePrefixWithSlash() + original.id + '/clone', clone);
   }
 
   remove(compte_divisionnaire: CompteDivisionnaire) {
-    return this.httpSrv.delete(this.getRoutePrefixWithSlash()+compte_divisionnaire.id);
+    return this.httpSrv.delete(this.getRoutePrefixWithSlash() + compte_divisionnaire.id);
   }
 
   removeSelection(compte_divisionnaires: CompteDivisionnaire[]) {
-    return this.httpSrv.deleteMultiple(this.getRoutePrefixWithSlash()+'delete-selection/',compte_divisionnaires);
+    return this.httpSrv.deleteMultiple(this.getRoutePrefixWithSlash() + 'delete-selection/', compte_divisionnaires);
   }
 
   public getRoutePrefix(): string {
@@ -45,7 +45,10 @@ export class CompteDivisionnaireService {
   }
 
   private getRoutePrefixWithSlash(): string {
-    return this.routePrefix+'/';
+    return this.routePrefix + '/';
   }
 
+  findBySousClasse(id: number) {
+    return this.httpSrv.get(this.getRoutePrefixWithSlash() + id + '/sous-classe');
+  }
 }
